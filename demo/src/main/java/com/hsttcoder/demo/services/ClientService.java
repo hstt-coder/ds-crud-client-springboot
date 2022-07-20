5b6f2eb68b722f256b64071af54a2b27a5e0bbaa
hstt-coder <eduardolopes19@outlook.com>
2022-07-20 20:00:06 -0300

    GET find by id, POST insert OK

--- demo/src/main/java/com/hsttcoder/demo/services/ClientService.java
+++ demo/src/main/java/com/hsttcoder/demo/services/ClientService.java
@@ -13,33 +13,44 @@ import com.hsttcoder.demo.entities.Client;
 import com.hsttcoder.demo.repositories.ClientRepository;
 import com.hsttcoder.demo.services.exceptions.ResourceNotFoundException;
 
-
-
-
-
-
-
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	ClientRepository repository;
-	
-	
+
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
-		
+
 		return list.map(x -> new ClientDTO(x));
 	}
-	
-	
+
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		
+
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
-		
+
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+
+		entity = repository.save(entity);
+
+		return new ClientDTO(entity);
+
+	}
+
+	public void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
 	}
 }
